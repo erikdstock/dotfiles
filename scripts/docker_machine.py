4c108b13c8000c603923f05222d13881c3fc222e
Erik Stockmeier <erik.d.stock@gmail.com>
2016-06-06 13:55:50 -0500

    add docker_machine powerline script (not used yet)

---
+++ scripts/docker_machine.py
@@ -0,0 +1,8 @@
+import os
+
+BG = 31  # blueish
+FG = 15  # white
+
+def add_docker_machine_segment(powerline):
+        powerline.append(' %s  %s ' % (u'\U0001f433', os.getenv('DOCKER_MACHINE_NAME')), FG, BG)
+
