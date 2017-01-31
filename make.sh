0179fc8c97ac126926fb9e373afe6d72722ef00c
Erik Stockmeier <erik.d.stock@gmail.com>
2017-01-30 21:53:34 -0500

    add pryrc

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile profile vimrc vim zshrc bash-it irbrc gitignore"    # list of files/folders to symlink in homedir
+files="bash_profile profile vimrc vim zshrc bash-it irbrc gitignore pryrc"    # list of files/folders to symlink in homedir
 
 ##########
 
