222eb84779fe5813c29ffc59bddcb74e005c9f90
Erik Stockmeier <erik.d.stock@gmail.com>
2017-03-11 14:14:56 -0500

    update global gitignore

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -5,6 +5,6 @@ alias be="bundle exec"
 alias yolo="be rake db:drop && be rake db:create && be rake db:migrate && be rake db:seed"
 alias showpath='echo "${PATH//:/$'"'\n'}\""
 alias gitroot="cd \$(git rev-parse --show-toplevel)"
-
+alias denv="eval \$(docker-machine env)"
 # MBA Structure
 alias godev="cd ~/dev"
