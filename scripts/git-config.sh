4d70e9e3fbe630f1c565cde267a864348145a3be
ErikS <erik@artsymail.com>
2019-01-14 15:02:34 -0500

    new plog command

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
 
 # git config --global user.name "Erik"
 # git config --global user.email "erik@artsymail.com"
@@ -12,5 +11,6 @@ git config --global alias.commend 'commit --amend --no-edit'
 git config --global alias.it '!git init && git commit -m "Initial commit." --allow-empty'
 git config --global alias.s 'status --short --branch'
 git config --global alias.grog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
+git config --global alias.plog 'log --pretty=oneline --abbrev-commit'
 git config --global alias.st 'status'
 git config --global alias.co 'checkout'
