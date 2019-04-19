e565645c11ca66787947bcadd82af449cfa52ebb
Erik Stockmeier <erik.d.stock@gmail.com>
2019-04-19 07:05:56 -0400

    latest updates from home

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,16 +1,12 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
+# These are already set in homedir/gitconfig
 
-
-# git config --global user.name "Erik"
-# git config --global user.email "erik@artsymail.com"
-# git config --global push.default simple
-
-# https://hackernoon.com/lesser-known-git-commands-151a1918a60#.qm8moxzcy
-git config --global alias.please 'push --force-with-lease'
-git config --global alias.commend 'commit --amend --no-edit'
-git config --global alias.it '!git init && git commit -m "Initial commit." --allow-empty'
-git config --global alias.s 'status --short --branch'
-git config --global alias.grog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
-git config --global alias.plog 'log --pretty=oneline --abbrev-commit'
-git config --global alias.st 'status'
-git config --global alias.co 'checkout'
+# # https://hackernoon.com/lesser-known-git-commands-151a1918a60#.qm8moxzcy
+# git config --global alias.please 'push --force-with-lease'
+# git config --global alias.commend 'commit --amend --no-edit'
+# git config --global alias.it '!git init && git commit -m "Initial commit." --allow-empty'
+# git config --global alias.s 'status --short --branch'
+# git config --global alias.grog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
+# git config --global alias.plog 'log --pretty=oneline --abbrev-commit'
+# git config --global alias.st 'status'
+# git config --global alias.co 'checkout'
