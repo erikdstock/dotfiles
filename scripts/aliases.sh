695ae8b9f295cce89be4232a7413b46a7a45b898
Erik Stockmeier <erik.d.stock@gmail.com>
2019-12-06 14:35:19 -0500

    more cleanups

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 # echo "Sourcing aliases"
-alias e="subl"
 alias be="bundle exec"
 alias berf="bundle exec rubocop -a"
 alias yolo="be rake db:drop && be rake db:create && be rake db:migrate && be rake db:seed"
-alias precommitrbcop="git status -s | awk '{print $2}' | grep .rb$ | xargs bundle exec rubocop -a"
 alias showpath='echo "${PATH//:/$'"'\n'}\""
 alias gitroot="cd \$(git rev-parse --show-toplevel)"
 alias denv="eval \$(docker-machine env)"
-# MBA Structure
-alias godev="cd ~/dev"
-alias godot="cd ~/dotfiles"
 
+# Force ls to use colors (G) and use humanized file sizes (h)
+alias ls='ls -Gh'
+alias ll='ls -al'
