1128d105c823baa0a48dd0e95ad6a3f408a32770
Erik Stockmeier <erik.d.stock@gmail.com>
2020-05-17 19:49:22 -0400

    add poetry to path

--- scripts/path.sh
+++ scripts/path.sh
@@ -1,1 +1,14 @@
+
+# Path for brew
 export PATH="/usr/local/sbin:$PATH"
+test -d /usr/local/bin && export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
+# Path for Heroku
+test -d /usr/local/heroku/ && export PATH="/usr/local/heroku/bin:$PATH"
+
+# Path to custom version of postgres:
+export PATH="/usr/local/opt/postgresql@9.6/bin:$PATH"
+
+# Path to poetry python dependency manager:
+export PATH="$HOME/.poetry/bin:$PATH"
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
